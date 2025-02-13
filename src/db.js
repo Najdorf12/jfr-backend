8e1a21ea3977d1a9aece63b084809d4fc3877de3
Najdorf <agustin.morro@gmail.com>
2025-02-13 11:41:44 -0300

    fix connection db

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import "dotenv/config.js";
 
 export const connectDB = async () => {
  /*  const MONGO = `${process.env.MONGO}`; */
- const MONGO = "mongodb+srv://agustinmorro:cytGbBr5PQdMiA0H@cluster0.4yegq.mongodb.net/"
+ const MONGO = "mongodb+srv://agustinmorro:cytGbBr5PQdMiA0H@cluster0.4yegq.mongodb.net"
   try {
     await mongoose.connect(MONGO);
     console.log("Conexion a mongo exitosa");
